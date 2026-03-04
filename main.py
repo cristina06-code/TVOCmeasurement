f03f55b17def5098bcf1ff9c727b3be92fc890e0
Cristina Muntean <202507008@post.au.dk>
2026-03-04 21:55:18 +0100

    fixing flask routes

--- main.py
+++ main.py
@@ -17,7 +17,14 @@ def index():
         TVOC = request.form.get('TVOC')
         eCO2 = request.form.get('eCO2')
         timestamp = request.form.get('timestamp')
+    return render_template('index.html', errors=errors)
+
+
+@app.route('/measurements')
+def measurements_list():
+    measurements = get_entries()
+    return render_template('measurementsList.html', measurements=measurements)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
